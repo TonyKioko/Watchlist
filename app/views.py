6bc904cd3e55cca963d6f5a46263a34ae29ae5db
TonyKioko <tonykioko384@gmail.com>
2018-08-27 16:54:06 +0300

    add macros

--- app/views.py
+++ app/views.py
@@ -8,8 +8,10 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+
     # Getting popular movie
     popular_movies = get_movies('popular')
-    print(popular_movies)
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title,popular = popular_movies)
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
