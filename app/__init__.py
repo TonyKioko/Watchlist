5a46234fece4fcb97d1239afb669c0f1ae92f10c
TonyKioko <tonykioko384@gmail.com>
2018-09-05 14:23:26 +0300

    simplemd added

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_simplemde import SimpleMDE
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -12,8 +13,14 @@ login_manager.login_view = 'auth.login'
 from flask_mail import Mail
 
 
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+simple = SimpleMDE()
+
+
+
+
 
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
@@ -43,7 +50,10 @@ def create_app(config_name):
     # configure UploadSet
     configure_uploads(app,photos)
     mail.init_app(app)
+    simple.init_app(app)
 
     # Will add the views and forms
+    
+    
 
     return app
--- config.py
+++ config.py
@@ -18,6 +18,9 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tony:1234@localhost/watchlist_test'
