775019c211e5b4e4fee00d9fedc7251abf9052dc
TonyKioko <tonykioko384@gmail.com>
2018-09-05 12:05:31 +0300

    upto date

--- app/auth/views.py
+++ app/auth/views.py
@@ -52,3 +52,5 @@ def register():
 #         return redirect(url_for('auth.login'))
 #         title = "New Account"
 #     return render_template('auth/register.html',registration_form = form)
+
+#Everything up to date
